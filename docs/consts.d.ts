43fda3c2e88120507706d8890e6da341abefdf4a
wang0618 <wang0.618@qq.com>
2026-02-15 06:41:27 +0000

    Build Sun Feb 15 06:41:27 UTC 2026

---
+++ docs/consts.d.ts
@@ -0,0 +1,3 @@
+export declare const PYTHON_VERSION = "3.13";
+export declare const PYWEBIO_VERSION = "1.8.4";
+//# sourceMappingURL=consts.d.ts.map
---
+++ docs/consts.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PYWEBIO_VERSION = exports.PYTHON_VERSION = void 0;
+exports.PYTHON_VERSION = '3.13';
+exports.PYWEBIO_VERSION = '1.8.4';
+//# sourceMappingURL=consts.js.map
